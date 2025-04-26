7408417e064bac18c2887ad93ae8455ed96202a5
Defauld <achifeiqra@gamil.com>
2025-04-26 19:59:00 +0400

    homework

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
+import { StudentComponent } from './student/student.component';
+import { UsersComponent } from './users/users.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'main', component: MainComponent},
+    {path: 'student', component: StudentComponent},
+    {path: 'users', component: UsersComponent},
+    {path: 'users/:id', component: UserDetailsComponent}
+
+];
---
+++ src/app/user-details/user-details.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import { user } from '../Model/user';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-details',
+  imports: [CommonModule],
+  templateUrl: './user-details.component.html',
+  styleUrl: './user-details.component.scss'
+})
+export class UserDetailsComponent {
+
+  public user?: user
+  
+    public usersData: user[] = [
+        {
+          "id": 7,
+          "email": "michael.lawson@reqres.in",
+          "first_name": "Michael",
+          "last_name": "Lawson",
+          "avatar": "https://reqres.in/img/faces/7-image.jpg"
+        },
+        {
+          "id": 8,
+          "email": "lindsay.ferguson@reqres.in",
+          "first_name": "Lindsay",
+          "last_name": "Ferguson",
+          "avatar": "https://reqres.in/img/faces/8-image.jpg"
+        },
+        {
+          "id": 9,
+          "email": "tobias.funke@reqres.in",
+          "first_name": "Tobias",
+          "last_name": "Funke",
+          "avatar": "https://reqres.in/img/faces/9-image.jpg"
+        },
+        {
+          "id": 10,
+          "email": "byron.fields@reqres.in",
+          "first_name": "Byron",
+          "last_name": "Fields",
+          "avatar": "https://reqres.in/img/faces/10-image.jpg"
+        },
+        {
+          "id": 11,
+          "email": "george.edwards@reqres.in",
+          "first_name": "George",
+          "last_name": "Edwards",
+          "avatar": "https://reqres.in/img/faces/11-image.jpg"
+        },
+        {
+          "id": 12,
+          "email": "rachel.howell@reqres.in",
+          "first_name": "Rachel",
+          "last_name": "Howell",
+          "avatar": "https://reqres.in/img/faces/12-image.jpg"
+        }]
+    
+      constructor(private router: ActivatedRoute){
+        this.router.params.subscribe(params => {
+          console.log(params);
+          this.user = this.usersData.find(user => user.id == params['id']);
+        })
+      }
+}
+
